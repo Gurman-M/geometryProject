ff273872ecaeba4e57965b158fa5dc6101f03fc0
Gurman Mundi <gurmanmundi10@gmail.com>
2021-11-14 08:13:35 -0500

    Create duplicatePointsException.java

---
+++ duplicatePointsException.java
@@ -0,0 +1,8 @@
+package geometry_improvement;
+
+public class duplicatePointsException extends Exception{
+	
+	public duplicatePointsException(String message) {
+		super(message);
+	}
+}//End of class
