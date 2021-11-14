7a64587e8f667426c9d1a2ea4bd887514972f6fd
Gurman Mundi <gurmanmundi10@gmail.com>
2021-11-14 08:15:04 -0500

    Create Shape.java

---
+++ Shape.java
@@ -0,0 +1,12 @@
+package geometry_improvement;
+
+public interface Shape {
+	//add methods common for each shape
+	//add area and perimeter methods
+
+	public double getArea(double radius);
+
+	public double getPerimeter(double radius); 
+	//each class that implements the interface must have these methods
+
+}//End of class
