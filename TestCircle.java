02e1ec8c8eb30def5f3d57c005c1c35cfd480b12
Gurman Mundi <gurmanmundi10@gmail.com>
2021-11-14 08:15:45 -0500

    Create TestCircle.java

---
+++ TestCircle.java
@@ -0,0 +1,15 @@
+package geometry_improvement;
+
+public class TestCircle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+		Circle c = new Circle (6.0); //sets to radius 6.0
+		System.out.println(c);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+	}
+}//End of class
